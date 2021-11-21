36fb5480cf847b2cc34856d14508e9cfcbb290b4
Shokochoco <shsuzu1992@gmail.com>
2021-11-21 21:03:22 +0900

    Initial Commit

---
+++ Rxswift+MVVMPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rxswift+MVVMPractice
+//
+//  Created by 鈴木淳子 on 2021/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
